de6199f4d64fe2f081e1fd1e85517e372bd1c56d
darrensisk <darrensisk1995@yahoo.ie>
2016-04-28 21:22:45 +0100

    Lighting HTTP Request

--- app/src/main/java/com/example/darren1/hms/LightingActivity.java
+++ app/src/main/java/com/example/darren1/hms/LightingActivity.java
@@ -1,12 +1,29 @@
 package com.example.darren1.hms;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 
 public class LightingActivity extends AppCompatActivity {
+
+    private boolean on;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +32,12 @@ public class LightingActivity extends AppCompatActivity {
 
 
         findAllViewsById();
+
+
+
+
+
+
     }
 
 
@@ -24,6 +47,7 @@ public class LightingActivity extends AppCompatActivity {
     private void findAllViewsById(){
 
         Button back = (Button) findViewById(R.id.backButton);
+        Button light = (Button) findViewById(R.id.switch2);
         back.setOnClickListener(new View.OnClickListener() {
 
 
@@ -33,5 +57,63 @@ public class LightingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        light.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                new lightSwitch().execute();
+            }
+        });
+
+
     }
+
+
+
+          private class lightSwitch extends AsyncTask <Void, Void, Void>{
+
+
+
+              @Override
+              protected Void doInBackground(Void... params) {
+                  String URL = " ";
+                  HttpURLConnection con;
+                  StringBuffer sb = new StringBuffer();
+
+
+                  try {
+                      con = (HttpURLConnection) new URL("http://" + URL + "/lights").openConnection();
+                      con.setRequestMethod("GET");
+
+                      BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                      String nextLine = "";
+
+                      while ((nextLine = reader.readLine()) != null){
+                          sb.append(nextLine);
+                      }
+
+
+                  } catch (MalformedURLException e){
+                      Log.e("ERROR", "MALFORMED URL EXCEPTION OCCURRED ON: " + e.toString());
+                  } catch (IOException e){
+                      Log.e("ERROR", "IO EXCEPTION OCCURRED ON: " + e.toString());
+                  }
+
+                  return null;
+              }
+
+
+    }
+
+
+
+
+
+
+
+
 }
